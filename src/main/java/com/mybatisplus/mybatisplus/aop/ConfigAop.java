134d7e01fea16b7823851a73cd2c0cdc40f7b9eb
zmy <2315293476@qq.com>
2019-06-06 13:38:48 +0800

    AOP控制日志打印

--- src/main/java/com/mybatisplus/mybatisplus/aop/ConfigAop.java
+++ src/main/java/com/mybatisplus/mybatisplus/aop/ConfigAop.java
@@ -7,8 +7,19 @@ import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.aop.Advisor;
+import org.springframework.aop.aspectj.AspectJExpressionPointcut;
+import org.springframework.aop.support.DefaultPointcutAdvisor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.transaction.TransactionDefinition;
+import org.springframework.transaction.interceptor.*;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * AOP控制类
@@ -17,12 +28,69 @@ import org.springframework.context.annotation.Configuration;
 @Aspect
 @Configuration
 public class ConfigAop {
-    public static final Logger logger =  LoggerFactory.getLogger(ConfigAop.class);
+    private static final Logger LOGGER =  LoggerFactory.getLogger(ConfigAop.class);
+    private static final int TX_METHOD_TIMEOUT = 5000;
+    private static final String AOP_POINTCUT_AROUND = "execution(* com.mybatisplus.mybatisplus.mapper.*.*(..))";
+    private static final String AOP_POINTCUT_EXPRESSION = "(execution (* com.mybatisplus.mybatisplus.mapper.*.*(..)))" +
+            " || (execution (* com.mybatisplus.mybatisplus.mapper.*.*(..)))";
+
+    @Autowired
+    private DataSourceTransactionManager transactionManager;
+
+    @Bean
+    public TransactionInterceptor txAdvice() {
+        NameMatchTransactionAttributeSource source = new NameMatchTransactionAttributeSource();
+        /*只读事务，不做更新操作*/
+        RuleBasedTransactionAttribute readOnlyTx = new RuleBasedTransactionAttribute();
+        readOnlyTx.setReadOnly(true);
+        readOnlyTx.setPropagationBehavior(TransactionDefinition.PROPAGATION_NOT_SUPPORTED);
+        /*当前存在事务就使用当前事务，当前不存在事务就创建一个新的事务*/
+        RuleBasedTransactionAttribute requiredTx = new RuleBasedTransactionAttribute();
+        requiredTx.setRollbackRules(Collections.singletonList(new RollbackRuleAttribute(Exception.class)));
+        requiredTx.setPropagationBehavior(TransactionDefinition.PROPAGATION_REQUIRED);
+        requiredTx.setTimeout(TX_METHOD_TIMEOUT);
+        Map<String, TransactionAttribute> txMap = new HashMap<>();
+
+        txMap.put("save*", requiredTx);
+        txMap.put("remove*", requiredTx);
+        txMap.put("update*", requiredTx);
+        txMap.put("batch*", requiredTx);
+        txMap.put("clear*", requiredTx);
+        txMap.put("add*", requiredTx);
+        txMap.put("append*", requiredTx);
+        txMap.put("modify*", requiredTx);
+        txMap.put("edit*", requiredTx);
+        txMap.put("insert*", requiredTx);
+        txMap.put("delete*", requiredTx);
+        txMap.put("do*", requiredTx);
+        txMap.put("create*", requiredTx);
+        /*select,count开头的方法,开启只读,提高数据库访问性能*/
+        txMap.put("select*", readOnlyTx);
+        txMap.put("get*", readOnlyTx);
+        txMap.put("valid*", readOnlyTx);
+        txMap.put("list*", readOnlyTx);
+        txMap.put("count*", readOnlyTx);
+        txMap.put("find*", readOnlyTx);
+        txMap.put("load*", readOnlyTx);
+        txMap.put("search*", readOnlyTx);
+        txMap.put("*", requiredTx);
+
+        source.setNameMap(txMap);
+        TransactionInterceptor txAdvice = new TransactionInterceptor(transactionManager, source);
+        return txAdvice;
+    }
+
+    @Bean
+    public Advisor txAdviceAdvisor() {
+        AspectJExpressionPointcut pointcut = new AspectJExpressionPointcut();
+        pointcut.setExpression(AOP_POINTCUT_EXPRESSION);
+        return new DefaultPointcutAdvisor(pointcut, txAdvice());
+    }
 
     /**
      * 定义一个切入点
      */
-    @Pointcut(value = "execution(* com.mybatisplus.mybatisplus.mapper.*.*(..))")
+    @Pointcut(value = AOP_POINTCUT_AROUND)
     public void getLogger(){
 
     }
@@ -36,11 +104,11 @@ public class ConfigAop {
     @Around(value = "getLogger()")
     public Object around(ProceedingJoinPoint joinPoint) throws Throwable {
         long startTime = getTime();
-        logger.info("访问开始时间："+startTime);
+        LOGGER.info("访问开始时间："+startTime);
         Object proceed = joinPoint.proceed();
         long endTime = getTime();
-        logger.info("访问结束时间："+endTime);
-        logger.info("访问返回总用时："+(endTime-startTime));
+        LOGGER.info("访问结束时间："+endTime);
+        LOGGER.info("访问返回总用时："+(endTime-startTime));
         return proceed;
     }
 
